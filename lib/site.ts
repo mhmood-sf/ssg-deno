290c2ae8b126fe834be2ed475d1c1ed05de4d0c4
quintik <28828855+quintik@users.noreply.github.com>
2023-03-11 02:20:19 -0500

    fmt: 4 spaces for indentation

--- lib/site.ts
+++ lib/site.ts
@@ -2,121 +2,120 @@ import { Plugin, SiteData, SiteDataOptions } from "./types.ts";
 import * as utils from "./utils.ts";
 
 // Create a new SiteData object.
-export function create(opts: SiteDataOptions): SiteData {
-  return {
-    url: opts ? opts.url : "localhost",
+export function create(opts?: SiteDataOptions): SiteData {
+    return {
+        url: opts ? opts.url : "localhost",
 
-    path: opts ? opts.path : Deno.cwd(),
+        path: opts ? opts.path : Deno.cwd(),
 
-    assets: [],
+        assets: [],
 
-    content: new Map(),
+        content: new Map(),
 
-    templates: new Map(),
+        templates: new Map(),
 
-    pages: new Map(),
+        pages: new Map(),
 
-    output: new Map(),
+        output: new Map(),
 
-    // We include a default parser for HTML files.
-    parser: new Map([
-      [
-        ".html",
-        (file) => ({
-          content: file[0],
-          data: new Map(Object.entries(file[1])),
-        }),
-      ],
-    ]),
+        // We include a default parser for HTML files.
+        parser: new Map([
+            [
+                ".html",
+                (file) => ({
+                    content: file[0],
+                    data: new Map(Object.entries(file[1])),
+                }),
+            ],
+        ]),
 
-    data: new Map(),
+        data: new Map(),
 
-    async load(): Promise<void> {
-      const cwd = this.path;
+        async load(): Promise<void> {
+            const cwd = this.path;
 
-      // Load content files.
-      const contentPaths = utils.recReadDirSync(cwd + "/content");
+            // Load content files.
+            const contentPaths = utils.recReadDirSync(cwd + "/content");
 
-      for (const path of contentPaths) {
-        // Get ext name and slice off the '.'
-        const ext = utils.path.extname(path);
+            for (const path of contentPaths) {
+                // Get ext name and slice off the '.'
+                const ext = utils.path.extname(path);
 
-        // If we have a parser for that extension then we store
-        // the file into the content map.
-        if (this.parser.has(ext)) {
-          const info = Deno.statSync(path);
-          const contents = Deno.readTextFileSync(path);
+                // If we have a parser for that extension then we store
+                // the file into the content map.
+                if (this.parser.has(ext)) {
+                    const info = Deno.statSync(path);
+                    const contents = Deno.readTextFileSync(path);
 
-          const key = path.slice((cwd + "/content").length);
-          this.content.set(key, [contents, info]);
-        }
-      }
+                    const key = path.slice((cwd + "/content").length);
+                    this.content.set(key, [contents, info]);
+                }
+            }
 
-      // Load template files.
-      const templatePaths = utils.recReadDirSync(cwd + "/templates");
+            // Load template files.
+            const templatePaths = utils.recReadDirSync(cwd + "/templates");
 
-      for (const path of templatePaths) {
-        const ext = utils.path.extname(path);
-        if (ext === ".ts") {
-          const template = (await import(path)).default;
-          const key = path.slice((cwd + "/templates").length);
-          this.templates.set(key, template);
-        }
-      }
-    },
+            for (const path of templatePaths) {
+                const ext = utils.path.extname(path);
+                if (ext === ".ts") {
+                    const template = (await import(path)).default;
+                    const key = path.slice((cwd + "/templates").length);
+                    this.templates.set(key, template);
+                }
+            }
+        },
 
-    parse(): void {
-      for (const [path, file] of this.content.entries()) {
-        const ext = utils.path.extname(path);
-        const parse = this.parser.get(ext);
+        parse(): void {
+            for (const [path, file] of this.content.entries()) {
+                const ext = utils.path.extname(path);
+                const parse = this.parser.get(ext);
 
-        if (parse !== undefined) {
-          const page = parse(file);
-          this.pages.set(path, page);
-        }
-      }
-    },
+                if (parse !== undefined) {
+                    const page = parse(file);
+                    this.pages.set(path, page);
+                }
+            }
+        },
 
-    build(): void {
-      // put pagedata into templates and set stuff into output.
-      for (const [path, page] of this.pages.entries()) {
-        const ext = utils.path.extname(path);
-        // Replace page extension with .ts extension.
-        const templatePath = path.slice(0, path.length - ext.length);
-        const pageTemplate = this.templates.get(templatePath + ".ts");
+        build(): void {
+            // put pagedata into templates and set stuff into output.
+            for (const [path, page] of this.pages.entries()) {
+                const ext = utils.path.extname(path);
+                // Replace page extension with .ts extension.
+                const templatePath = path.slice(0, path.length - ext.length);
+                const pageTemplate = this.templates.get(templatePath + ".ts");
 
-        if (pageTemplate !== undefined) {
-          const outputPath = templatePath + ".html";
-          const outputHtml = pageTemplate(this, page);
-          this.output.set(outputPath, outputHtml);
-          continue;
-        }
+                if (pageTemplate !== undefined) {
+                    const outputPath = templatePath + ".html";
+                    const outputHtml = pageTemplate(this, page);
+                    this.output.set(outputPath, outputHtml);
+                    continue;
+                }
 
-        const defaultPath = utils.path.join(
-          utils.path.dirname(path),
-          "_default.ts",
-        );
-        const defaultTemplate = this.templates.get(defaultPath);
+                const defaultPath = utils.path.join(
+                    utils.path.dirname(path),
+                    "_default.ts",
+                );
+                const defaultTemplate = this.templates.get(defaultPath);
 
-        if (defaultTemplate !== undefined) {
-          const outputPath = templatePath + ".html";
-          const outputHtml = defaultTemplate(this, page);
-          this.output.set(outputPath, outputHtml);
-          continue;
-        }
+                if (defaultTemplate !== undefined) {
+                    const outputPath = templatePath + ".html";
+                    const outputHtml = defaultTemplate(this, page);
+                    this.output.set(outputPath, outputHtml);
+                    continue;
+                }
 
-        throw new Error("No template found for: " + path);
-      }
-    },
+                throw new Error("No template found for: " + path);
+            }
+        },
 
-    apply(plugin: Plugin): SiteData {
-      plugin(this);
-      return this;
-    },
+        apply(plugin: Plugin): SiteData {
+            plugin(this);
+            return this;
+        },
 
-    // TODO: Write this properly!
-    qualifiedUrlFor(resource: string): string {
-      return this.path + "/output/" + resource;
-    },
-  };
+        qualifiedUrlFor(resource: string): string {
+            return resource;
+        },
+    };
 }
--- lib/types.ts
+++ lib/types.ts
@@ -8,14 +8,14 @@ type FileContents = string;
 // URL. Similar to pages in most other SSGs. The content is
 // required, other data may be attached as needed.
 export type PageData = {
-  content: string;
-  data: Map<string, any>;
+    content: string;
+    data: Map<string, any>;
 };
 
 // Initial options for the SiteData object.
 export type SiteDataOptions = {
-  url: string;
-  path: string;
+    url: string;
+    path: string;
 };
 
 // Templates have access to the site data and the current page
@@ -36,64 +36,64 @@ export type Parser = (input: FileTuple) => PageData;
 export type Plugin = (site: SiteData) => void;
 
 export type SiteData = {
-  // Base url for the site.
-  url: string;
+    // Base url for the site.
+    url: string;
 
-  // Path to the directory containing the site's config.ts
-  path: string;
+    // Path to the directory containing the site's config.ts
+    path: string;
 
-  // List of paths to asset/static files that will later be copied over
-  // directly to the output directory, at the root of the website.
-  // TODO: Unimplemented!
-  assets: string[];
+    // List of paths to asset/static files that will later be copied over
+    // directly to the output directory, at the root of the website.
+    // TODO: Unimplemented!
+    assets: string[];
 
-  // Content files, usually just the .md files for the site content, as
-  // well as any other files that need to be included in the output. Those
-  // with parsers are processed, others are ignored. Files prefixed with
-  // an underscore are ignored entirely.
-  content: Map<FilePath, FileTuple>;
+    // Content files, usually just the .md files for the site content, as
+    // well as any other files that need to be included in the output. Those
+    // with parsers are processed, others are ignored. Files prefixed with
+    // an underscore are ignored entirely.
+    content: Map<FilePath, FileTuple>;
 
-  // Templates are .ts files that export a function taking in
-  // SiteData and PageData and returning an HTML string. For each
-  // page in the content directory, there must be a matching
-  // template for producing the output. In each (sub)directory,
-  // a `_default.ts` template can be provided as a fallback.
-  templates: Map<FilePath, Template>;
+    // Templates are .ts files that export a function taking in
+    // SiteData and PageData and returning an HTML string. For each
+    // page in the content directory, there must be a matching
+    // template for producing the output. In each (sub)directory,
+    // a `_default.ts` template can be provided as a fallback.
+    templates: Map<FilePath, Template>;
 
-  // Every content file is parsed into a PageData object and
-  // stored in this list.
-  pages: Map<FilePath, PageData>;
+    // Every content file is parsed into a PageData object and
+    // stored in this list.
+    pages: Map<FilePath, PageData>;
 
-  // Output HTML files are stored here after processing. These
-  // are then written to the filesystem.
-  output: Map<FilePath, FileContents>;
+    // Output HTML files are stored here after processing. These
+    // are then written to the filesystem.
+    output: Map<FilePath, FileContents>;
 
-  // Parsers are used for processing content files into PageData
-  // objects. Also determine which content files are read. Those
-  // that don't have a parser are ignored.
-  parser: Map<FileType, Parser>;
+    // Parsers are used for processing content files into PageData
+    // objects. Also determine which content files are read. Those
+    // that don't have a parser are ignored.
+    parser: Map<FileType, Parser>;
 
-  // We allow adding any other data needed to the SiteData object.
-  data: Map<string, any>;
+    // We allow adding any other data needed to the SiteData object.
+    data: Map<string, any>;
 
-  // Loads content files/templates into the SiteData object.
-  load: () => Promise<void>;
+    // Loads content files/templates into the SiteData object.
+    load: () => Promise<void>;
 
-  // Parses content files into PageData objects.
-  parse: () => void;
+    // Parses content files into PageData objects.
+    parse: () => void;
 
-  // Builds the site, populating the output field with HTML data.
-  build: () => void;
+    // Builds the site, populating the output field with HTML data.
+    build: () => void;
 
-  // Applies a plugin function to the SiteData.
-  apply: (plugin: Plugin) => SiteData;
+    // Applies a plugin function to the SiteData.
+    apply: (plugin: Plugin) => SiteData;
 
-  // Returns the full absolute URL for a resource.
-  // The input string must be an absolute path from the root
-  // of the project directory. This function will then
-  // prepend the absolute file system path (for the
-  // local webserver) or the website address for production
-  // builds. This method should be used instead of manually
-  // inserting links into templates.
-  qualifiedUrlFor: (resource: string) => string;
+    // Returns the full absolute URL for a resource.
+    // The input string must be an absolute path from the root
+    // of the project directory. This function will then
+    // prepend the absolute file system path (for the
+    // local webserver) or the website address for production
+    // builds. This method should be used instead of manually
+    // inserting links into templates.
+    qualifiedUrlFor: (resource: string) => string;
 };
