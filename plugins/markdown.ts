3c976fed0ad68c3398759f440747e079e82eeb67
quintik <28828855+quintik@users.noreply.github.com>
2023-04-10 00:32:26 -0400

    feat: add frontmatter support in the .md plugin

--- plugins/markdown.ts
+++ plugins/markdown.ts
@@ -1,9 +1,33 @@
 import { renderMarkdown } from "https://deno.land/x/markdown_renderer/mod.ts";
+import { extract, test } from "https://deno.land/std/front_matter/any.ts";
 import { SiteData } from "../mod.ts";
 
 export default (site: SiteData) => {
-    site.parser.set(".md", (file) => ({
-        content: renderMarkdown(file[0], site.data.get("markdown-options")),
-        data: new Map(Object.entries(file[1])),
-    }));
+    site.parser.set(".md", (file) => {
+        const pageData = {
+            content: "",
+            data: new Map(Object.entries(file[1]))
+        }
+
+        if (test(file[0])) {
+            const extracted = extract(file[0]);
+            const frontMatter = extracted.attrs;
+
+            for (const [key, val] of Object.entries(frontMatter)) {
+                pageData.data.set(key, val);
+            }
+
+            pageData.content = renderMarkdown(
+                extracted.body,
+                site.data.get("markdown-options")
+            );
+        } else {
+            pageData.content = renderMarkdown(
+                file[0],
+                site.data.get("markdown-options")
+            );
+        }
+
+        return pageData;
+    });
 };
